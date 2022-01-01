23e3bb2b17e2505283d0e5f8db54086f1aa74064
Şükrü Taha Bıyık <57368706+ultimate-dev@users.noreply.github.com>
2022-01-02 01:10:58 +0300

    Initial commit

---
+++ Esp8266_HTTP.cpp
@@ -0,0 +1,65 @@
+#include <Esp8266_HTTP.h>
+
+boolean Esp::ready()
+{
+    EspSerial.println("AT");
+    while (!EspSerial.find("OK"))
+        EspSerial.println("AT");
+    return true;
+}
+
+boolean Esp::clientMode()
+{
+    if (ready())
+    {
+        EspSerial.println("AT+CWMODE=1");
+        while (!EspSerial.find("OK"))
+            EspSerial.println("AT+CWMODE=1");
+        return true;
+    }
+    else
+        return false;
+}
+
+boolean Esp::connect(String ssid, String pass)
+{
+    if (clientMode())
+    {
+        EspSerial.println("AT+CWJAP=\"" + ssid + "\",\"" + pass + "\"");
+        while (!EspSerial.find("OK"))
+            return true;
+    }
+    else
+        return false;
+}
+
+boolean Esp::startHttp(String ip, String port)
+{
+    EspSerial.println("AT+CIPSTART=\"TCP\",\"" + ip + "\"," + port);
+    if (EspSerial.find("OK"))
+        return true;
+    else
+        return false;
+}
+
+boolean Esp::http(String uri, String method)
+{
+    String data = method + " /" + uri + "/\r\n\r\n";
+    EspSerial.print("AT+CIPSEND=");
+    EspSerial.println(data.length() + 2);
+    delay(2000);
+    if (EspSerial.find(">"))
+    {
+        EspSerial.println(data);
+        delay(1000);
+        return false;
+    }
+    else
+        return false;
+}
+
+boolean Esp::closeHttp()
+{
+    EspSerial.println("AT+CIPCLOSE");
+    return true;
+}
---
+++ Esp8266_HTTP.h
@@ -0,0 +1,29 @@
+#include <SoftwareSerial.h>
+
+#ifndef Esp8266_HTTP_H
+#define Esp8266_HTTP_H
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+class SoftwareSerial;
+class Esp
+{
+private:
+  SoftwareSerial EspSerial;
+
+public:
+  Esp(uint8_t rx, uint8_t tx) : EspSerial(rx, tx)
+  {
+    EspSerial.begin(115200);
+  };
+  boolean ready();
+  boolean clientMode();
+  boolean connect(String ssid, String pass);
+  boolean startHttp(String ip, String port);
+  boolean http(String uri, String method);
+  boolean closeHttp();
+};
+#endif
